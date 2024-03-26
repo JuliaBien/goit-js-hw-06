21ef8ab1c150fabcaff9dca4422cb3c1aa619fc4
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-03-26 17:25:48 +0100

    update

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputRange = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = `${inputRange.value}px`;
 const sizeChanging = event => {
   text.style.fontSize = `${event.currentTarget.value}px`;
 };
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputAmountOfBoxes = document.querySelector('input');
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+let boxSize = 20;
+function createBoxes() {
+  const amountofBoxes = inputAmountOfBoxes.value;
+  for (let i = 0; i < amountofBoxes; i++) {
+    const randomColor = getRandomHexColor();
+    boxSize += 10;
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = randomColor;
+    boxesContainer.append(box);
+  }
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+  boxSize = 20;
+}
+buttonCreate.addEventListener('click', createBoxes);
+buttonDestroy.addEventListener('click', destroyBoxes);
